3495617a7505a9a467187ab4b096005dceb4914e
Wasib <87130748+Wasib2005@users.noreply.github.com>
2024-10-02 13:57:29 +0600

    All necessary pages added

---
+++ src/Pages/ErrorElement.jsx
@@ -0,0 +1,9 @@
+const ErrorElement = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default ErrorElement;
---
+++ src/Pages/FirstPage.jsx
@@ -0,0 +1,11 @@
+
+
+const FirstPage = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default FirstPage;
