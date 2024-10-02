1230f7c89d14d937a31baf4dab9438f21364d0dc
Wasib <87130748+Wasib2005@users.noreply.github.com>
2024-10-02 14:15:14 +0600

    Home added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,18 @@
+import { useLoaderData } from "react-router-dom";
 
+import Banners from "../Comp/BannerComp/Banners";
+import HomeBooks from "../Comp/HomeBook/HomeBooks";
 
 const Home = () => {
+  const booksData = useLoaderData();
   return (
     <div>
-      
+      <div>
+        <Banners booksData={booksData} />
+      </div>
+      <div>
+        <HomeBooks bookData={booksData}/>
+      </div>
     </div>
   );
 };
