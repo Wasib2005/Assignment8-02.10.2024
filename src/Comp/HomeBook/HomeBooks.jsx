e12ff497628296f99801a45bf266c6ec6040d97e
Wasib <87130748+Wasib2005@users.noreply.github.com>
2024-10-02 14:23:15 +0600

    HomeBook list added

---
+++ src/Comp/HomeBook/HomeBooks.jsx
@@ -0,0 +1,11 @@
+import PropTypes from "prop-types";
+
+const HomeBooks = ({ bookData }) => {
+  return <div></div>;
+};
+
+HomeBooks.propTypes = {
+  bookData: PropTypes.array.isRequired,
+};
+
+export default HomeBooks;
