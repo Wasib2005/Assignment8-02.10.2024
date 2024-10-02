a04d440d16066539311ae3f8463b8cb968b89f54
Wasib <87130748+Wasib2005@users.noreply.github.com>
2024-10-02 14:17:04 +0600

    home page banners component added

---
+++ src/Comp/Banners.jsx
@@ -0,0 +1,16 @@
+import PropTypes from "prop-types";
+
+
+const Banners = ({ booksData }) => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+Banners.propTypes = {
+  booksData: PropTypes.array,
+};
+
+export default Banners;
