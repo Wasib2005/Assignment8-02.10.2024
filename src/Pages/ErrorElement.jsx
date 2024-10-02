15e616925de875f6aafbc15b131a61c3c814f04f
Wasib <87130748+Wasib2005@users.noreply.github.com>
2024-10-02 14:10:06 +0600

    error page added

--- src/Pages/ErrorElement.jsx
+++ src/Pages/ErrorElement.jsx
@@ -1,7 +1,31 @@
+import { Link } from "react-router-dom";
+
 const ErrorElement = () => {
   return (
     <div>
-      
+      <section className="flex items-center h-full p-16 dark:bg-gray-50 dark:text-gray-800">
+        <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
+          <div className="max-w-md text-center">
+            <h2 className="mb-8 font-extrabold text-9xl text-error">
+              <span className="sr-only">Error</span>404
+            </h2>
+            <p className="text-2xl font-semibold md:text-3xl">
+              Sorry, we couldn't find this page.
+            </p>
+            <p className="mt-4 mb-8 dark:text-gray-600">
+              But dont worry, you can go back
+            </p>
+            <Link
+            to={-1}
+              rel="noopener noreferrer"
+              href="#"
+              className="btn btn-success"
+            >
+              Back to homepage
+            </Link>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
