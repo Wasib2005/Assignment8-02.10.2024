db9eb6826b09919d5ed3d7fec38f5e790af3e1b5
Wasib <87130748+Wasib2005@users.noreply.github.com>
2024-10-02 14:14:29 +0600

    model completed & Footer completed

--- src/Pages/FirstPage.jsx
+++ src/Pages/FirstPage.jsx
@@ -1,9 +1,15 @@
-
+import { Outlet } from "react-router-dom";
+import Nav from "./Nav";
+import Footer from "./Footer";
 
 const FirstPage = () => {
   return (
     <div>
-      
+      <Nav />
+      <div className="ml-6 mr-6 min-w-[640px] min-h-[calc(100vh-(48px+373px))]">
+        <Outlet />
+      </div>
+      <Footer />
     </div>
   );
 };
