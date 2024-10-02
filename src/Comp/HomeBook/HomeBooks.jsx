2ff1983e2961ed6daf7cf0579d5f5910e4d76326
Wasib <87130748+Wasib2005@users.noreply.github.com>
2024-10-02 14:49:59 +0600

    home section completed

--- src/Comp/HomeBook/HomeBooks.jsx
+++ src/Comp/HomeBook/HomeBooks.jsx
@@ -1,7 +1,16 @@
 import PropTypes from "prop-types";
-
-const HomeBooks = ({ bookData }) => {
-  return <div></div>;
+import HomeBook from "./HomeBook";
+const HomeBooks = ({ bookData: booksData }) => {
+  return (
+    <div className="mt-40 text-center">
+      <h1 className="text-3xl font-bold mb-9">Books</h1>
+      <div className=" grid grid-cols-2 gap-4 md:gap-6 md:grid-cols-3">
+        {booksData.slice(14,23).map((book, i) => (
+          <HomeBook book={book} key={i} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 HomeBooks.propTypes = {
