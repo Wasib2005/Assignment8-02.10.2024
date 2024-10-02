b0b75ed8288d2422900c1130edc743b4fe994eab
Wasib <87130748+Wasib2005@users.noreply.github.com>
2024-10-02 14:19:18 +0600

    banner for each book added!!!

---
+++ src/Comp/Banner.jsx
@@ -0,0 +1,15 @@
+import PropTypes from "prop-types";
+
+const Banner = ({ book }) => {
+  return (
+    
+    <div>
+      
+    </div>
+  );
+};
+
+Banner.propTypes = {
+  book: PropTypes.object,
+};
+
--- src/Comp/Banners.jsx
+++ src/Comp/Banners.jsx
@@ -1,10 +1,14 @@
 import PropTypes from "prop-types";
-
+import Banner from "./Banner";
 
 const Banners = ({ booksData }) => {
   return (
-    <div>
-
+    <div className="flex justify-center">
+      <div className="carousel rounded-box m-auto min-w-[640px] border">
+        {booksData.map((book, i) => (
+          <Banner book={book} key={i} />
+        ))}
+      </div>
     </div>
   );
 };
