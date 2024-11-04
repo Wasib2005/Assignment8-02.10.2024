14c6355d4863ec107cb793160d597addaf2face4
Wasib2005 <wasib_2005@proton.me>
2024-11-04 20:38:55 +0600

    Chart1 updated

--- src/Pages/PagesToRead.jsx
+++ src/Pages/PagesToRead.jsx
@@ -8,10 +8,9 @@ const PagesToRead = () => {
   const [readListBookId, setReadListBookId] = useState([]);
   const [readListBookObj, setReadListBookObj] = useState([]);
   const [chartReadListBookObj, setChartReadListBookObj] = useState([]);
-
-  useEffect(() => {
-    setReadListBookId(takeFromLS("readBookID"));
-  }, []);
+  const [watchListBookId, setWatchListBookId] = useState([]);
+  const [watchListBookObj, setWatchListBookObj] = useState([]);
+  const [chartWatchListBookObj, setChartWatchListBookObj] = useState([]);
 
   useEffect(() => {
     const storedReadList = takeFromLS("readBookID");
@@ -22,7 +21,7 @@ const PagesToRead = () => {
       );
       setReadListBookObj(tempBooks);
     }
-  }, []);
+  }, [books]);
 
   useEffect(() => {
     setChartReadListBookObj(
@@ -34,29 +33,73 @@ const PagesToRead = () => {
     );
   }, [readListBookObj]);
 
-  console.log(chartReadListBookObj);
+  useEffect(() => {
+    const storedWatchList = takeFromLS("watchListBookId");
+    if (storedWatchList) {
+      setWatchListBookId(storedWatchList);
+      const tempBooks = books.filter((book) =>
+        storedWatchList.includes(book.isbn13)
+      );
+      setWatchListBookObj(tempBooks);
+    }
+  }, [books]);
+
+  useEffect(() => {
+    setChartWatchListBookObj(
+      watchListBookObj.map((book, index) => ({
+        id: index,
+        label: book.name,
+        value: book.totalPages,
+      }))
+    );
+  }, [watchListBookObj]);
 
   return (
     <div className="flex justify-center h-[100vh] border items-center">
-      <PieChart
-        series={[
-          {
-            data: chartReadListBookObj,
-            innerRadius: 80,
-            outerRadius: 150,
-            paddingAngle: 1,
-            cornerRadius: 5,
-            startAngle: 0,
-            endAngle: 360,
-            cx: 150,
-            cy: 150,
-            faded: { innerRadius: 30, additionalRadius: -30, color: "gray" },
-            highlightScope: { fade: 'global', highlight: 'item' },
-          },
-        ]}
-        width={400}
-        height={400}
-      />
+      <div className="flex flex-col justify-center items-center">
+        <PieChart
+          series={[
+            {
+              data: chartReadListBookObj,
+              innerRadius: 80,
+              outerRadius: 150,
+              paddingAngle: 1,
+              cornerRadius: 5,
+              startAngle: 0,
+              endAngle: 360,
+              cx: 150,
+              cy: 150,
+              faded: { innerRadius: 30, additionalRadius: -30, color: "gray" },
+              highlightScope: { fade: "global", highlight: "item" },
+            },
+          ]}
+          width={400}
+          height={400}
+        />
+        <h3 className="text-3xl">Your Read List</h3>
+      </div>
+      <div className="flex flex-col justify-center items-center">
+        <PieChart
+          series={[
+            {
+              data: chartWatchListBookObj,
+              innerRadius: 80,
+              outerRadius: 150,
+              paddingAngle: 1,
+              cornerRadius: 5,
+              startAngle: 0,
+              endAngle: 360,
+              cx: 150,
+              cy: 150,
+              faded: { innerRadius: 30, additionalRadius: -30, color: "gray" },
+              highlightScope: { fade: "global", highlight: "item" },
+            },
+          ]}
+          width={400}
+          height={400}
+        />
+        <h3 className="text-3xl">Your Wishlist</h3>
+      </div>
     </div>
   );
 };
